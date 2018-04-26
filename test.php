c87cef0fce04272b0c719c279b7055adacce831e
comenxviii <32865861+comenxviii@users.noreply.github.com>
2018-04-26 16:24:42 +0700

    add form

--- test.php
+++ test.php
@@ -1,4 +1,30 @@
 <?php include "header.php" ?>
 
 
+<section id="maincontent">
+    <div class="container">
+      <div class="row">
+        <div class="span12">
+          <h3>จัดการเอกสาร</h3>
+          <div class="row">
+            <form>
+              <fieldset>
+                <legend>กรอกข้อมูล</legend>
+                <label>ชื่อโครงการ</label>
+                <input type="text" placeholder="">
+                <label>จำนวนเงิน</label>
+                <input type="text" placeholder="">
+                <label>ชื่อผู้เเจ้ง</label>
+                <input type="text" placeholder="">
+                <br><br>
+                <button type="submit" class="btn">ตกลง</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
 <?php include "footer.php" ?>
