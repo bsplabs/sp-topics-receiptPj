f81aabe115de09c21dcd3873f9b00f12d3dcfcec
comenxviii <32865861+comenxviii@users.noreply.github.com>
2018-04-26 17:12:37 +0700

    add

--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php include "header.php" ?>
 
-
 <section id="maincontent">
     <div class="container">
       <div class="row">
@@ -11,13 +10,13 @@
               <fieldset>
                 <legend>กรอกข้อมูล</legend>
                 <label>ชื่อโครงการ</label>
-                <input type="text" id="nameProject">
+                <input type="text" id="nameProject" value="โครงการ A">
                 <label>จำนวนเงิน</label>
-                <input type="text" id="amount">
+                <input type="text" id="amount" value="1,000,000">
                 <label>ชื่อผู้เเจ้ง</label>
-                <input type="text" id="nameOwn">
+                <input type="text" id="nameOwn" value="นายชัชวาล สารชัย">
                 <br><br>
-                <button type="submit" class="btn">ตกลง</button>
+                <button type="submit" class="btn" id="btn">ตกลง</button>
               </fieldset>
             </form>
           </div>
@@ -26,14 +25,31 @@
     </div>
   </section>
 <!-- ######################  -->
-  <script>
-    function chk()
-    {
-      var nameProject = document.getElementById('nameProject').value;
-      var nameProject = document.getElementById('nameProject').value;
-      var nameProject = document.getElementById('nameProject').value;
 
-    }
-  </script>
+
+
+<script>
+  $("button").click(function(){
+      var nameProject = document.getElementById('nameProject').value;
+      var amount = document.getElementById('amount').value;
+      var nameOwn = document.getElementById('nameOwn').value;
+      var dataString = {
+        "nameProject": nameProject,
+        "amount": amount,
+        "nameOwn": nameOwn
+      };
+
+      $.ajax({
+        type:"post",
+        url:"showdata.php",
+        data: dataString,
+        cache: false,
+        success: function(result){
+          alert("Success");
+          window.location = "showdata.php";
+        }
+      });
+  });
+</script>
 
 <?php include "footer.php" ?>
