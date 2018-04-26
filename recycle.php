093cf571793e0f9a53e2e557cba357e7df972222
comenxviii <32865861+comenxviii@users.noreply.github.com>
2018-04-26 15:44:47 +0700

    chang someone

---
+++ recycle.php
@@ -0,0 +1,60 @@
+<div class="container">
+  <div class="row">
+    <div class="span3">
+      <div class="widget">
+        <!-- logo -->
+        <div class="footerlogo">
+          <h6><a href="index.html">:: Receipt ::</a></h6>
+          <!-- <img src="assets/img/logo.png" alt="" /> -->
+        </div>
+        <!-- end logo -->
+        <address>
+    <strong>Special Topics Class</strong><br>
+     Computer Engineering, NU<br>
+     Pitsanulok, TH 65000<br>
+    <abbr title="Phone">P:</abbr> (+66) 88-3014335 </address>
+      </div>
+    </div>
+    <div class="span3">
+      <div class="widget">
+        <h5>Browse pages</h5>
+        <ul class="list list-ok">
+          <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+          <li><a href="#">Tamquam ponderum at eum, nibh dicta offendit mei</a></li>
+          <li><a href="#">Vix no vidisse dolores intellegam</a></li>
+          <li><a href="#">Est virtute feugiat accommodare eu</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="span3">
+      <div class="widget">
+        <h5>Flickr photostream</h5>
+        <div class="flickr_badge">
+          <script type="text/javascript" src="http://www.flickr.com/badge_code_v2.gne?count=8&amp;display=random&amp;size=s&amp;layout=x&amp;source=user&amp;user=34178660@N03"></script>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+    <div class="span3">
+      <div class="widget">
+        <h5>Keep updated</h5>
+        <p>
+          ป้อนอีเมลของคุณเพื่อสมัครรับจดหมายข่าว
+        </p>
+        <form>
+          <div class="input-append">
+            <input class="span2" id="appendedInputButton" type="text">
+            <button class="btn btn-color" type="submit">ติดตาม</button>
+          </div>
+        </form>
+        <ul class="social-network">
+          <li><a href="#"><i class="icon-bg-light icon-facebook icon-circled icon-1x"></i></a></li>
+          <li><a href="#" title="Twitter"><i class="icon-bg-light icon-twitter icon-circled icon-1x"></i></a></li>
+          <li><a href="#" title="Linkedin"><i class="icon-bg-light icon-linkedin icon-circled icon-1x"></i></a></li>
+          <li><a href="#" title="Pinterest"><i class="icon-bg-light icon-pinterest icon-circled icon-1x"></i></a></li>
+          <li><a href="#" title="Google plus"><i class="icon-bg-light icon-google-plus icon-circled icon-1x"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
