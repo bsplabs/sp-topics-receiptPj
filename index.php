8c42a01e156054ebd64faa08ca55e562ae763241
comenxviii <32865861+comenxviii@users.noreply.github.com>
2018-05-14 02:26:24 +0700

    new

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>หน้าเเรก</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+		<style>
+				.navbar {
+					background-color: #c80113;
+					padding-left: 15%;
+					padding-right: 15%;
+				}
+				span, .navbar-brand {
+					font-size: 24px;
+					color: white;
+					font-weight: 500;
+					border-top: 3px solid white;
+					border-bottom: 3px solid white;
+				}
+				li, a {
+					color: white;
+					font-size: 16px;
+				}
+		</style>
+
+	</head>
+	<hr style="margin: 0; border-color:black;">
+		<nav class="navbar navbar-expand-lg">
+			<img src="./img/a.png" width="50" height="50">
+			<span class="navbar-brand" href="#">RECEIPT2PDF</span>
+			<ul class="navbar-nav ml-auto">
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">LOGIN</a>
+		    </li>
+		    <!-- <li class="nav-item">
+		      <a class="nav-link" href="#">รายการของฉัน</a>
+		    </li>
+		    <li class="nav-item">
+		      <a class="nav-link" href="#">อัพโหลด</a>
+		    </li> -->
+			</ul>
+		</nav>
+		<hr style="margin: 0; border-color:black;">
+		<!-- <div class="container-fluid">
+			<h1>PDF2Receip</h1>
+			<div class="text-right">LOGIN</div>
+			<div class="text-center">
+				<h1>NuNew Isarapon คนสวย</h1>
+			</div>
+		</div> -->
+	<body>
+
+	</body>
+</html>
