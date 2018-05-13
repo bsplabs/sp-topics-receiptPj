6444b03dc40baf1d5ccee925400452b521306905
comenxviii <32865861+comenxviii@users.noreply.github.com>
2018-05-14 05:17:55 +0700

    new2

---
+++ footer.php
@@ -0,0 +1,5 @@
+      <footer class="blog-footer">
+        <p>How to <a href="https://getbootstrap.com/">RECEIPT TO PDF </a> presented by <a href="https://twitter.com/mdo">ขนมปัง สังขยา</a></p>
+      </footer>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,66 +1,51 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>หน้าเเรก</title>
-
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
-
-		<!-- jQuery library -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-		<!-- Popper JS -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-
-		<!-- Latest compiled JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-
-		<style>
-				.navbar {
-					background-color: #c80113;
-					padding-left: 15%;
-					padding-right: 15%;
-				}
-				span, .navbar-brand {
-					font-size: 24px;
-					color: white;
-					font-weight: 500;
-					border-top: 3px solid white;
-					border-bottom: 3px solid white;
-				}
-				li, a {
-					color: white;
-					font-size: 16px;
-				}
-		</style>
-
-	</head>
-	<hr style="margin: 0; border-color:black;">
-		<nav class="navbar navbar-expand-lg">
-			<img src="./img/a.png" width="50" height="50">
-			<span class="navbar-brand" href="#">RECEIPT2PDF</span>
-			<ul class="navbar-nav ml-auto">
-		    <li class="nav-item">
-		      <a class="nav-link" href="#">LOGIN</a>
-		    </li>
-		    <!-- <li class="nav-item">
-		      <a class="nav-link" href="#">รายการของฉัน</a>
-		    </li>
-		    <li class="nav-item">
-		      <a class="nav-link" href="#">อัพโหลด</a>
-		    </li> -->
-			</ul>
-		</nav>
-		<hr style="margin: 0; border-color:black;">
-		<!-- <div class="container-fluid">
-			<h1>PDF2Receip</h1>
-			<div class="text-right">LOGIN</div>
-			<div class="text-center">
-				<h1>NuNew Isarapon คนสวย</h1>
+<?php include "header.php"; ?>
+	<div class="jumbotron jumbotron-fluid">
+	  <div class="container">
+	    <h1>จัดการเอกสารอย่างง่าย</h1>
+	    <p>RECEIPT2PDF คือเว็บไซต์จัดการเอกสาร เช่น ใบสำคัญรับเงิน หนังสือราชการ บันทึกข้อความและยังสามารถอัพโหลดใบเสร็จใว้ได้ด้วย</p>
+	  </div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md first">
+				<img src="./img/method1.png" width="200" height="220">
+				<span class="method1"><b>ขั้นตอน</b> การจัดการเอกสาร</span>
+				<p>
+				<ol>
+					<li>สมัครสมาชิกและลงชื่อเข้าใช้งาน</li>
+					<li>เลือกรูปบบเอกสารที่จะสร้าง</li>
+					<li>กรอกข้อมูลผ่านฟอร์มให้ครบถ้วน</li>
+					<li>เลือกเป็นไฟล์ PDF</li>
+					<li>กด Dowload หรือ Print</li>
+					<li>opsdksdsdlap[sda[spd</li>
+					<li>opsdksdsdlap[sda[spd</li>
+					<li>opsdksdsdlap[sda[spd</li>
+					<li>opsdksdsdlap[sda[spd</li>
+				</ol>
+				</p>
 			</div>
-		</div> -->
-	<body>
+			<div class="col-md second">
+				<img src="./img/method2.png" width="200" height="216">
+				<span class="method2"><b>ขั้นตอน</b> การอัพโหลดไฟล์</span>
+				<p>
+				<ol>
+					<li>สมัครสมาชิกและลงชื่อเข้าใช้งาน</li>
+					<li>เลือกรูปบบเอกสารที่จะสร้าง</li>
+					<li>กรอกข้อมูลผ่านฟอร์มให้ครบถ้วน</li>
+					<li>เลือกเป็นไฟล์ PDF</li>
+					<li>กด Dowload หรือ Print</li>
+					<li>opsdksdsdlap[sda[spd</li>
+					<li>opsdksdsdlap[sda[spd</li>
+					<li>opsdksdsdlap[sda[spd</li>
+					<li>opsdksdsdlap[sda[spd</li>
+				</ol>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row" style="height:150px; background-color: #e9ecef; margin-top: 2%;">
 
-	</body>
-</html>
+		</div>
+	</div>
+<?php include "footer.php"; ?>
