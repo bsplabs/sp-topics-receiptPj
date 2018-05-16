af9746ad5ab775abdc855c5828d7fb09db46461a
Jae_panupong <panupongc57@email.nu.ac.th>
2018-05-16 22:15:50 +0700

    update

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>หน้าเเรก</title>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Itim" rel="stylesheet">
+
+		<link rel="stylesheet" href="./css/style.css">
+
+	</head>
+		<hr style="margin: 0; border-color:black;">
+		<nav class="navbar navbar-expand-lg">
+			<span class="navbar-brand" href="#">RECEIPT</span>
+			<img src="./img/4.png" width="50" height="50">
+			<span class="navbar-brand" href="#">PDF</span>
+			<ul class="navbar-nav ml-auto">
+		    <li class="nav-item">
+					<button type="button" class="btn btn-secondary">Log in</button>
+		    </li>
+		    <li class="nav-item">
+		      <button type="button" class="btn btn-secondary">Sign in</button>
+		    </li>
+			</ul>
+		</nav>
+		<hr style="margin: 0; border:1.5px solid black;">
+
+	<body>
